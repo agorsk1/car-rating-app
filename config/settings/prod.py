2188357fb9b8101ef822b02deb2f163d6eff8c5b
agorsk1 <artq1406@gmail.com>
2021-02-08 19:36:44 +0100

    Add django-heroku

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,9 @@
 from .base import *
 
+import django_heroku
+
 ALLOWED_HOSTS = ['artur-gorski-car-app.herokuapp.com']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
