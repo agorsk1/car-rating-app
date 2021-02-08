b44b6164b09e75b0c462bb869f5d479f2d90bb53
agorsk1 <artq1406@gmail.com>
2021-02-08 19:18:06 +0100

    Add prod settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from typing import List
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
---
+++ config/settings/prod.py
@@ -0,0 +1,5 @@
+from .base import *
+
+ALLOWED_HOSTS = ['artur-gorski-car-app.herokuapp.com']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
